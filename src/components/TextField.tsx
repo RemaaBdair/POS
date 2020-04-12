71f780ea95d7a060029d1ae53576d46a31fbb7d7
RemaaBdair <RemaaBdair@users.noreply.github.com>
2020-04-12 15:22:24 +0300

    update the style using theme in textfield component

--- src/components/TextField.tsx
+++ src/components/TextField.tsx
@@ -1,30 +1,29 @@
-import React from "react";
+import React, { FunctionComponent } from "react";
 import TextField from "@material-ui/core/TextField";
-import { withStyles, createStyles } from "@material-ui/core/styles";
+import { createMuiTheme } from "@material-ui/core/styles";
+import {
+  withStyles,
+  createStyles,
+  ThemeProvider,
+} from "@material-ui/core/styles";
 interface Props {
   type: string;
   labelName: string;
   classes: {
     root: string;
-    cssLabel: string;
-    cssFocused: string;
     notchedOutline: string;
   };
 }
+const theme = createMuiTheme({
+  palette: {
+    primary: {
+      main: "#1861ab",
+    },
+  },
+});
 const styles = createStyles({
   root: {
     "&:hover:not($disabled):not($focused):not($error) $notchedOutline": {
-      borderColor: "#1861ab",
-      borderWidth: 2,
-    },
-  },
-  cssLabel: {
-    "&$cssFocused": {
-      color: "#1861ab",
-    },
-  },
-  cssFocused: {
-    "&$cssFocused $notchedOutline": {
       borderColor: "#1861ab",
       borderWidth: 2,
     },
@@ -34,28 +33,24 @@ const styles = createStyles({
   focused: {},
   error: {},
 });
-function HigherOrderComponent(props: Props) {
+const HigherOrderComponent: FunctionComponent<Props> = (props) => {
   const { classes, labelName, type } = props;
   return (
-    <TextField
-      id="outlined-basic"
-      type={type}
-      label={labelName}
-      variant="outlined"
-      InputProps={{
-        classes: {
-          root: classes.root,
-          focused: classes.cssFocused,
-          notchedOutline: classes.notchedOutline,
-        },
-      }}
-      InputLabelProps={{
-        classes: {
-          root: classes.cssLabel,
-          focused: classes.cssFocused,
-        },
-      }}
-    />
+    <ThemeProvider theme={theme}>
+      <TextField
+        id="outlined-basic"
+        type={type}
+        label={labelName}
+        color="primary"
+        variant="outlined"
+        InputProps={{
+          classes: {
+            root: classes.root,
+            notchedOutline: classes.notchedOutline,
+          },
+        }}
+      />
+    </ThemeProvider>
   );
-}
+};
 export const MyTextField = withStyles(styles)(HigherOrderComponent);
