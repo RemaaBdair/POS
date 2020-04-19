ea25bc2c6a3f63b6ad3fb5870d9110ae71f6e79d
RemaaBdair <RemaaBdair@users.noreply.github.com>
2020-04-19 15:56:07 +0300

    move styles to a separate file and put all header relevants files  into one folder

---
+++ src/Components/Header/styles.tsx
@@ -0,0 +1,35 @@
+import { createStyles } from "@material-ui/core/styles";
+import Background from "../whiteBackground.png";
+export const styles = createStyles({
+  "@global": {
+    body: {
+      fontFamily: "lato",
+      backgroundColor: "white",
+      backgroundImage: `url(${Background})`,
+      height: "100%",
+    },
+  },
+  root: {
+    flexGrow: 1,
+    flexDirection: "row",
+    alignItems: "center",
+  },
+  toolBar: { justifyContent: "center" },
+  logoImg: {
+    height: 35,
+    width: 135,
+  },
+  logoContainer: {
+    alignSelf: "center",
+  },
+  appBar: {
+    backgroundColor: "white",
+    flexDirection: "row",
+    justifyContent: "space-evenly",
+  },
+  listItem: {
+    margin: 5,
+    display: "flex",
+    alignItems: "center",
+  },
+});
