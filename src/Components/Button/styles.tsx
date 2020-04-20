bc3f4b56c7efc7c723983e243d7ebfcdcbc6357f
RemaaBdair <RemaaBdair@users.noreply.github.com>
2020-04-20 21:36:54 +0300

    remove color and bacground rules

--- src/Components/Button/styles.tsx
+++ src/Components/Button/styles.tsx
@@ -1,9 +1,7 @@
 import { createStyles } from "@material-ui/core/styles";
-import { Props } from "./Button";
+//import { Props } from "./Button";
 export const styles = createStyles({
   root: {
-    background: (props: Props) => props.bgColor,
-    color: (props: Props) => props.color,
     borderRadius: 0,
     textTransform: "none",
     width: "100%",
