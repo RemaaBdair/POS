e6912595c0b006f92c76bc3b79b4596771de309d
RemaaBdair <RemaaBdair@users.noreply.github.com>
2020-04-20 19:16:51 +0300

    add main theme to the project

--- src/index.tsx
+++ src/index.tsx
@@ -5,13 +5,27 @@ import LogIn from "./LogIn";
 import { Router } from "@reach/router";
 import MainPage from "./MainPage";
 import * as serviceWorker from "./serviceWorker";
+import { ThemeProvider, createMuiTheme } from "@material-ui/core/styles";
+
+const theme = createMuiTheme({
+  palette: {
+    primary: {
+      main: "#777",
+    },
+    secondary: {
+      main: "#1861ab",
+    },
+  },
+});
 
 ReactDOM.render(
   <React.StrictMode>
-    <Router>
-      <LogIn path="/" />
-      <MainPage path="/Main/" />
-    </Router>
+    <ThemeProvider theme={theme}>
+      <Router>
+        <LogIn path="/" />
+        <MainPage path="/Main/" />
+      </Router>
+    </ThemeProvider>
   </React.StrictMode>,
   document.getElementById("root")
 );
