74ba795bcc99e722d8bc8674277c906f28f146a1
RemaaBdair <RemaaBdair@users.noreply.github.com>
2020-04-20 19:45:15 +0300

    add peopleMenue and CategoryMenue t the storybook

--- src/Components/Header/header.stories.js
+++ src/Components/Header/header.stories.js
@@ -1,8 +1,11 @@
 import React from "react";
 import Header from "./Header";
+import CategoryMenu from "./CategoryMenu";
+import PeopleMenue from "./PeopleMenue";
 
 export default {
   title: "Header",
 };
-
+export const peopleMenue = () => <PeopleMenue />;
+export const categoryMenu = () => <CategoryMenu />;
 export const AppHeader = () => <Header />;
