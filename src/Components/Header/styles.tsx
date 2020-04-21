af00e5a9ced7edc4f07a8678bfc9d62d652d3d0a
RemaaBdair <RemaaBdair@users.noreply.github.com>
2020-04-21 15:29:23 +0300

    remove body rule

--- src/Components/Header/styles.tsx
+++ src/Components/Header/styles.tsx
@@ -1,14 +1,5 @@
 import { createStyles } from "@material-ui/core/styles";
-import Background from "../../whiteBackground.png";
 export const styles = createStyles({
-  "@global": {
-    body: {
-      fontFamily: "lato",
-      backgroundColor: "white",
-      backgroundImage: `url(${Background})`,
-      height: "100%",
-    },
-  },
   root: {
     flexGrow: 1,
     flexDirection: "row",
