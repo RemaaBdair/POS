3ee0b16425df9a5c3f0a33863abc4f2df29e0332
RemaaBdair <RemaaBdair@users.noreply.github.com>
2020-04-21 15:28:07 +0300

    check access token once the component is created

--- src/MainPage.tsx
+++ src/MainPage.tsx
@@ -1,10 +1,10 @@
-import React from "react";
+import React, { useEffect } from "react";
+import { navigate } from "@reach/router";
 import { WithStyles } from "@material-ui/core/styles";
 import { withStyles, createStyles } from "@material-ui/core/styles";
 import { RouteComponentProps } from "@reach/router";
-import Header from "./components/Header/Header";
 import Background from "./whiteBackground.png";
-
+import Header from "./components/Header/Header";
 const styles = createStyles({
   "@global": {
     body: {
@@ -18,6 +18,10 @@ const styles = createStyles({
 const MainPage: React.FunctionComponent<
   WithStyles<typeof styles> & RouteComponentProps
 > = (props) => {
+  const token = localStorage.getItem("LoggedIn");
+  useEffect(() => {
+    if (token !== "true") navigate("/");
+  });
   return <Header />;
 };
 export default withStyles(styles)(MainPage);
