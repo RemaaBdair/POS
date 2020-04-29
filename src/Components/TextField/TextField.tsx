195d56b94a35d129b8f19248fdd598c6e6875cdb
RemaaBdair <RemaaBdair@users.noreply.github.com>
2020-04-29 12:55:54 +0300

    add fullWidth,value and reuired prop to TextField component

--- src/Components/TextField/TextField.tsx
+++ src/Components/TextField/TextField.tsx
@@ -5,23 +5,38 @@ import { styles } from "./styles";
 interface Props {
   type: string;
   labelName: string;
-  OnChangehandle: (arg: string) => void;
-  errorText: string;
+  OnChangehandle?: (arg: string) => void;
+  errorText?: string;
+  value?: string;
+  required?: boolean;
+  fullWidth?: boolean;
 }
 const textField: React.FunctionComponent<Props & WithStyles<typeof styles>> = (
   props
 ) => {
-  const { classes, labelName, type, OnChangehandle, errorText } = props;
+  const {
+    classes,
+    labelName,
+    type,
+    OnChangehandle,
+    errorText,
+    fullWidth = true,
+    required = false,
+    value,
+  } = props;
   return (
     <TextField
       id="outlined-basic"
       type={type}
+      value={value ? value : ""}
       label={labelName}
       color="secondary"
       variant="outlined"
-      fullWidth
-      onChange={(event) => OnChangehandle(event.target.value)}
-      required
+      fullWidth={fullWidth}
+      onChange={(event) =>
+        OnChangehandle ? OnChangehandle(event.target.value) : null
+      }
+      required={required}
       helperText={errorText}
       error={errorText ? true : false}
       InputProps={{
