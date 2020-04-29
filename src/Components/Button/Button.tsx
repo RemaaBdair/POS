f4810be1b9e661cf8df0c0a594c37ec98c36d3ba
RemaaBdair <RemaaBdair@users.noreply.github.com>
2020-04-29 12:53:37 +0300

    add fullWidth,disable and variant property to Button component

--- src/Components/Button/Button.tsx
+++ src/Components/Button/Button.tsx
@@ -5,18 +5,29 @@ import { styles } from "./styles";
 export interface Props {
   type: "submit" | "button";
   children: string;
-  size: "large" | "medium" | "small";
+  size?: "large" | "medium" | "small";
+  disable?: boolean;
+  fullWidth?: boolean;
   variant: "contained" | "text" | "outlined";
   OnClickHandle: (e: React.SyntheticEvent) => void;
 }
 const button: React.FunctionComponent<Props & WithStyles<typeof styles>> = (
   props
 ) => {
-  const { children, classes, OnClickHandle } = props;
+  const {
+    children,
+    classes,
+    OnClickHandle,
+    size = "large",
+    disable = false,
+    fullWidth = true,
+  } = props;
   return (
     <Button
+      fullWidth={fullWidth}
+      disabled={disable}
       variant={props.variant}
-      size={props.size}
+      size={size}
       type={props.type}
       className={classes.root}
       onClick={OnClickHandle}
