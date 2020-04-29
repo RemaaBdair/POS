daaf8c00579bcdac8495457da00801ae73f56599
RemaaBdair <RemaaBdair@users.noreply.github.com>
2020-04-29 12:51:08 +0300

    move MainPage component into Main folder in pages

--- src/pages/Main/MainPage.tsx
+++ src/pages/Main/MainPage.tsx
@@ -1,20 +1,10 @@
 import React, { useEffect } from "react";
 import { navigate } from "@reach/router";
 import { WithStyles } from "@material-ui/core/styles";
-import { withStyles, createStyles } from "@material-ui/core/styles";
+import { withStyles } from "@material-ui/core/styles";
 import { RouteComponentProps } from "@reach/router";
-import Background from "./whiteBackground.png";
-import Header from "./components/Header/Header";
-const styles = createStyles({
-  "@global": {
-    body: {
-      fontFamily: "lato",
-      backgroundColor: "white",
-      backgroundImage: `url(${Background})`,
-      height: "100%",
-    },
-  },
-});
+import Header from "../../components/Header/Header";
+import { styles } from "./styles";
 const MainPage: React.FunctionComponent<
   WithStyles<typeof styles> & RouteComponentProps
 > = (props) => {
