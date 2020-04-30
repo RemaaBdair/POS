9e731e03f067d1c860990d21711c8a2e0981efd5
RemaaBdair <RemaaBdair@users.noreply.github.com>
2020-04-30 03:15:55 +0300

    add style to DeleteCategoryDialog

--- src/pages/Category/components/DeleteCategoryDialog/DeleteCategoryDialog.tsx
+++ src/pages/Category/components/DeleteCategoryDialog/DeleteCategoryDialog.tsx
@@ -1,33 +1,47 @@
 import React from "react";
+import { WithStyles, withStyles } from "@material-ui/core/styles";
 import Dialog from "@material-ui/core/Dialog";
 import DialogActions from "@material-ui/core/DialogActions";
 import DialogContent from "@material-ui/core/DialogContent";
 import DialogContentText from "@material-ui/core/DialogContentText";
 import DialogTitle from "@material-ui/core/DialogTitle";
-import { MyButton } from "../../../../components/Button/Button";
-import { deleteCategory } from "./util";
+import { MyButton } from "../../../../Components/Button/Button";
+import { deleteCategory } from "../CategoriesList/util";
+import { styles } from "./styles";
 interface Props {
   openDialog: boolean;
   handleClose: () => void;
   name: string;
   id: string;
 }
-export const DeleteCategoryDialog: React.FunctionComponent<Props> = (props) => {
+const DeleteCategoryDialog: React.FunctionComponent<
+  WithStyles<typeof styles> & Props
+> = (props) => {
+  const { classes } = props;
   const { openDialog, handleClose, name, id } = props;
   return (
     <Dialog
       open={openDialog}
       onClose={handleClose}
       aria-labelledby="form-dialog-title"
+      fullWidth
     >
-      <DialogTitle id="form-dialog-title">Delete Category</DialogTitle>
-      <DialogContent>
+      <DialogTitle id="form-dialog-title" classes={{ root: classes.title }}>
+        Delete Category
+      </DialogTitle>
+      <DialogContent classes={{ root: classes.content }}>
         <DialogContentText>
           Are you sure you want to deltete {name} category?
         </DialogContentText>
       </DialogContent>
-      <DialogActions>
-        <MyButton OnClickHandle={handleClose} type="submit" variant="text">
+      <DialogActions classes={{ root: classes.actions }}>
+        <MyButton
+          OnClickHandle={handleClose}
+          type="submit"
+          variant="text"
+          fullWidth={false}
+          classes={{ root: classes.button }}
+        >
           Cancel
         </MyButton>
         <MyButton
@@ -37,6 +51,8 @@ export const DeleteCategoryDialog: React.FunctionComponent<Props> = (props) => {
           }}
           type="submit"
           variant="contained"
+          fullWidth={false}
+          classes={{ root: classes.button }}
         >
           Yes
         </MyButton>
@@ -44,3 +60,4 @@ export const DeleteCategoryDialog: React.FunctionComponent<Props> = (props) => {
     </Dialog>
   );
 };
+export default withStyles(styles)(DeleteCategoryDialog);
