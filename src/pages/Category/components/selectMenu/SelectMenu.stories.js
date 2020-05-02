3e9863cb68b7b7d7158cb4e94705211acb770507
RemaaBdair <RemaaBdair@users.noreply.github.com>
2020-05-02 23:32:55 +0300

    remove unused components

--- src/pages/Category/components/selectMenu/SelectMenu.stories.js
+++
@@ -1,6 +0,0 @@
-import React from "react";
-import SelectMenu from "./SelectMenu";
-export default {
-  title: "CategoryPage",
-};
-export const selectMenu = () => <SelectMenu />;
--- src/pages/Category/components/selectMenu/styles.tsx
+++
@@ -1,6 +0,0 @@
-import { createStyles } from "@material-ui/core/styles";
-export const styles = createStyles({
-  label: {
-    padding: " 10px",
-  },
-});
