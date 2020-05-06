c0f083009482a54d4cc84bcde9b2407e02fbbd53
Rema'a Bdair <57008402+RemaaBdair@users.noreply.github.com>
2020-05-06 23:02:52 +0300

    add style to FilterGrid component

---
+++ src/pages/Products/Components/FilterGrid/styles.tsx
@@ -0,0 +1,8 @@
+import { createStyles } from "@material-ui/core/styles";
+export const styles = createStyles({
+  datePicker: { marginLeft: 16, marginTop: 0 },
+  submitButton: {
+    margin: "0px 0px 0px 16px",
+    padding: "8px 16px",
+  },
+});
