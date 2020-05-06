710c6143b03e77ad8f5e8d979c3a4d5da06798e0
Rema'a Bdair <57008402+RemaaBdair@users.noreply.github.com>
2020-05-06 23:00:54 +0300

    create ProductGrid component

---
+++ src/pages/Products/Components/ProductGrid/ProductGrid.tsx
@@ -0,0 +1,79 @@
+import React, { useState, useEffect } from "react";
+import { WithStyles, withStyles } from "@material-ui/core/styles";
+import Grid from "@material-ui/core/Grid";
+import { SvgIconProps } from "@material-ui/core/SvgIcon";
+import { RouteComponentProps } from "@reach/router";
+import ProductsList from "../ProductsList/ProductsList";
+import { MyTextField } from "../../../../Components/TextField/TextField";
+import { MyButton } from "../../../../Components/Button/Button";
+import FilterGrid from "../FilterGrid/FilterGrid";
+import { styles } from "./styles";
+import { Product, fetchProducts } from "../ProductsList/util";
+const ProductGrid: React.FunctionComponent<
+  WithStyles<typeof styles> & SvgIconProps & RouteComponentProps
+> = (props) => {
+  const { classes } = props;
+  const [productData, setProductData] = useState<Product[]>([]);
+  useEffect(() => {
+    fetchProducts().then((res) => setProductData(res));
+  }, []);
+  const [searchText, setSearchText] = useState("");
+  const handleSearchTextChange = (text: string) => {
+    setSearchText(text);
+  };
+  const [initDate, setSelectedInitDate] = React.useState<Date | null>(
+    new Date("2019-10-10")
+  );
+  const [endDate, setSelectedEndDate] = React.useState<Date | null>(
+    new Date("2025-02-15")
+  );
+
+  const handleInitDateChange = (date: Date | null) => {
+    setSelectedInitDate(date);
+  };
+  const handleEndDateChange = (date: Date | null) => {
+    setSelectedEndDate(date);
+  };
+  const onFetchProducts = () => {
+    fetchProducts().then((res) => setProductData(res));
+  };
+  return (
+    <Grid container className={classes.container}>
+      <Grid item xs={12} justify="flex-end" container>
+        <FilterGrid
+          initDate={initDate}
+          endDate={endDate}
+          handleInitDateChange={handleInitDateChange}
+          handleEndDateChange={handleEndDateChange}
+        />
+      </Grid>
+      <Grid item xs={6}>
+        <MyButton
+          OnClickHandle={() => {}}
+          type="submit"
+          variant="contained"
+          fullWidth={false}
+        >
+          Add Product
+        </MyButton>
+      </Grid>
+      <Grid item xs={6} justify="flex-end" container>
+        <MyTextField
+          fullWidth={false}
+          labelName="Search"
+          type="text"
+          value={searchText}
+          onChange={handleSearchTextChange}
+        />
+      </Grid>
+      <Grid item xs={12} justify="center" container>
+        <ProductsList
+          searchText={searchText}
+          productsData={productData}
+          onFetchProducts={onFetchProducts}
+        />
+      </Grid>
+    </Grid>
+  );
+};
+export default withStyles(styles)(ProductGrid);
