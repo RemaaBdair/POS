e6ea03af6b04f02affd2ed9216cf549b4aa3f1c2
Rema'a Bdair <57008402+RemaaBdair@users.noreply.github.com>
2020-05-06 22:55:56 +0300

    add fetchProducts function

--- src/pages/Products/Components/ProductsList/util.tsx
+++ src/pages/Products/Components/ProductsList/util.tsx
@@ -7,3 +7,11 @@ export interface Product {
   tax: string;
   expirationDate: string;
 }
+export type Order = "asc" | "desc";
+export const fetchProducts = async (): Promise<Product[]> => {
+  return await fetch("http://localhost:3001/products")
+    .then((response) => response.json())
+    .catch((error) => {
+      console.log(error);
+    });
+};
