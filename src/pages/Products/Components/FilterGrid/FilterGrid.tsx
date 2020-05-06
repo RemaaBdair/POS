8bc5e5cf590d0038154f848d07bcf1053de2f11a
Rema'a Bdair <57008402+RemaaBdair@users.noreply.github.com>
2020-05-06 23:02:29 +0300

    create FilterGrid component

---
+++ src/pages/Products/Components/FilterGrid/FilterGrid.tsx
@@ -0,0 +1,81 @@
+import "date-fns";
+import React from "react";
+import Grid from "@material-ui/core/Grid";
+import DateFnsUtils from "@date-io/date-fns";
+import {
+  MuiPickersUtilsProvider,
+  KeyboardDatePicker,
+} from "@material-ui/pickers";
+import Typography from "@material-ui/core/Typography";
+import { MyButton } from "../../../../Components/Button/Button";
+import { WithStyles, withStyles } from "@material-ui/core/styles";
+import { styles } from "./styles";
+interface Props {
+  initDate: Date | null;
+  endDate: Date | null;
+  handleInitDateChange: (date: Date | null) => void;
+  handleEndDateChange: (date: Date | null) => void;
+}
+export const FilterGrid: React.FunctionComponent<
+  WithStyles<typeof styles> & Props
+> = (props) => {
+  const {
+    classes,
+    initDate,
+    endDate,
+    handleInitDateChange,
+    handleEndDateChange,
+  } = props;
+
+  return (
+    <MuiPickersUtilsProvider utils={DateFnsUtils}>
+      <Grid container alignItems="center" justify="flex-end">
+        <Typography color="primary" component="span">
+          Expiration Date
+        </Typography>
+
+        <KeyboardDatePicker
+          disableToolbar
+          variant="inline"
+          format="yyyy-MM-dd"
+          margin="normal"
+          id="from-date"
+          label="From"
+          color="secondary"
+          className={classes.datePicker}
+          value={initDate}
+          onChange={handleInitDateChange}
+          KeyboardButtonProps={{
+            "aria-label": "change date",
+          }}
+        />
+
+        <KeyboardDatePicker
+          disableToolbar
+          variant="inline"
+          format="yyyy-MM-dd"
+          margin="normal"
+          id="to-date"
+          label="To"
+          color="secondary"
+          className={classes.datePicker}
+          value={endDate}
+          onChange={handleEndDateChange}
+          KeyboardButtonProps={{
+            "aria-label": "change date",
+          }}
+        />
+        <MyButton
+          OnClickHandle={() => {}}
+          type="submit"
+          variant="outlined"
+          classes={{ root: classes.submitButton }}
+          fullWidth={false}
+        >
+          Apply Filter
+        </MyButton>
+      </Grid>
+    </MuiPickersUtilsProvider>
+  );
+};
+export default withStyles(styles)(FilterGrid);
