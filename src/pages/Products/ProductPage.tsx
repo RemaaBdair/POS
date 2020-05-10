e797e66d299a34c398564e7668e2d2ebe8ff561c
Rema'a Bdair <57008402+RemaaBdair@users.noreply.github.com>
2020-05-11 02:22:54 +0300

    rename component

--- src/pages/Products/ProductPage.tsx
+++ src/pages/Products/ProductPage.tsx
@@ -1,10 +1,9 @@
 import React from "react";
 import Box from "@material-ui/core/Box";
-import { WithStyles, withStyles } from "@material-ui/core/styles";
 import { RouteComponentProps } from "@reach/router";
 import Header from "../../Components/Header/Header";
 import ProductGrid from "./Components/ProductGrid/ProductGrid";
-const CategoryPage: React.FunctionComponent<RouteComponentProps> = (props) => {
+const ProductPage: React.FunctionComponent<RouteComponentProps> = (props) => {
   // const { classes } = props;
   return (
     <Box>
@@ -13,4 +12,4 @@ const CategoryPage: React.FunctionComponent<RouteComponentProps> = (props) => {
     </Box>
   );
 };
-export default CategoryPage;
+export default ProductPage;
