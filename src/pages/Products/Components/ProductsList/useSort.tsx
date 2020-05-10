53efbbac74097de36c60cd4ca20cdd4027c92b01
Rema'a Bdair <57008402+RemaaBdair@users.noreply.github.com>
2020-05-11 02:23:55 +0300

    add custom hook for sorting functionality

---
+++ src/pages/Products/Components/ProductsList/useSort.tsx
@@ -0,0 +1,22 @@
+import React, { useState } from "react";
+import { Order } from "./util";
+const useSort = <T,>(
+  defaultOrderBy: keyof T,
+  data: T[],
+  sortData: (productData: T[], orderBy: keyof T, ascOrder: boolean) => T[]
+) => {
+  const [order, setOrder] = useState<Order>("asc");
+  const [orderBy, setOrderBy] = useState<keyof T>(defaultOrderBy);
+  const handleSort = (event: React.MouseEvent<unknown>, property: keyof T) => {
+    const isAsc = orderBy === property && order === "asc";
+    setOrder(isAsc ? "desc" : "asc");
+    setOrderBy(property);
+  };
+  const sortedData = React.useMemo(
+    () => sortData(data, orderBy, order === "asc" ? true : false),
+    [sortData, data, orderBy, order]
+  );
+
+  return { order, orderBy, sortedData, handleSort };
+};
+export default useSort;
