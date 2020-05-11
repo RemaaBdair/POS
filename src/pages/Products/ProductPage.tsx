0296c85b5bbe01a57143245857eb166486ab66d0
Rema'a Bdair <57008402+RemaaBdair@users.noreply.github.com>
2020-05-12 01:04:54 +0300

    remove header component

--- src/pages/Products/ProductPage.tsx
+++ src/pages/Products/ProductPage.tsx
@@ -1,13 +1,10 @@
 import React from "react";
 import Box from "@material-ui/core/Box";
 import { RouteComponentProps } from "@reach/router";
-import Header from "../../Components/Header/Header";
 import ProductGrid from "./Components/ProductGrid/ProductGrid";
 const ProductPage: React.FunctionComponent<RouteComponentProps> = (props) => {
-  // const { classes } = props;
   return (
     <Box>
-      <Header />
       <ProductGrid />
     </Box>
   );
