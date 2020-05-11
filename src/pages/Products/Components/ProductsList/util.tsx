fa5d377ae3355398981bd4000afdd902e7e907e2
Rema'a Bdair <57008402+RemaaBdair@users.noreply.github.com>
2020-05-11 08:43:17 +0300

    add filter function

--- src/pages/Products/Components/ProductsList/util.tsx
+++ src/pages/Products/Components/ProductsList/util.tsx
@@ -31,3 +31,59 @@ export const sortData = (
         : -1;
   });
 };
+type FilterOptions = "less than or equal" | "more than or equal";
+
+type FilterValues = {
+  filterOption1: FilterOptions;
+  filterValue1: string | null;
+  filter1By: keyof Product;
+  filterOption2: FilterOptions;
+  filterValue2: string | null;
+  filter2By: keyof Product;
+};
+const evaluate = (
+  filterOption: FilterOptions,
+  filterValue: string,
+  filterBy: keyof Product,
+  row: Product
+): boolean => {
+  let results = {
+    "less than or equal": function () {
+      return row[filterBy].toLowerCase() <= filterValue.toLowerCase();
+    },
+    "more than or equal": function () {
+      return row[filterBy].toLowerCase() >= filterValue.toLowerCase();
+    },
+  };
+  if (row[filterBy]) return results[filterOption]();
+  else return false;
+};
+export const filterData = (
+  filterValues: FilterValues,
+  rows: Product[]
+): Product[] => {
+  const {
+    filterOption1,
+    filter1By,
+    filterValue1,
+    filter2By,
+    filterValue2,
+    filterOption2,
+  } = filterValues;
+  if (!filterValue1 && !filterValue2) return rows;
+  else if (filterValue1 && filterValue2)
+    return rows.filter(
+      (row: Product) =>
+        evaluate(filterOption1, filterValue1, filter1By, row) &&
+        evaluate(filterOption2, filterValue2, filter2By, row)
+    );
+  else if (filterValue1 && !filterValue2)
+    return rows.filter((row: Product) =>
+      evaluate(filterOption1, filterValue1, filter1By, row)
+    );
+  else if (!filterValue1 && filterValue2)
+    return rows.filter((row: Product) =>
+      evaluate(filterOption2, filterValue2, filter2By, row)
+    );
+  else return rows;
+};
