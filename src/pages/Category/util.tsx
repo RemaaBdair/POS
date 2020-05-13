6a755412c498ed2065349069cf8ddd24991d1075
Rema'a Bdair <57008402+RemaaBdair@users.noreply.github.com>
2020-05-13 21:45:30 +0300

    move api's functions to a new file

--- src/pages/Category/util.tsx
+++ src/pages/Category/util.tsx
@@ -4,58 +4,6 @@ export interface Category {
   date: string;
 }
 export type Order = "asc" | "desc";
-export const fetchCategories = async (): Promise<Category[]> => {
-  return await fetch("http://localhost:3001/categories")
-    .then((response) => response.json())
-    .catch((error) => {
-      console.log(error);
-    });
-};
-export const editCategory = async (
-  newName: string,
-  category: Category
-): Promise<string> => {
-  const { id } = category;
-  return await fetch(`http://localhost:3001/categories/${id}`, {
-    method: "PUT",
-    headers: {
-      "Content-Type": "application/json",
-    },
-    body: JSON.stringify({ ...category, name: `${newName}` }),
-  })
-    .then(() => "success")
-    .catch(() => "failed");
-};
-export const createCategory = async (name: string): Promise<string> => {
-  const currentdate = new Date();
-  const date = [
-    currentdate.getFullYear(),
-    ("0" + (currentdate.getMonth() + 1)).slice(-2),
-    ("0" + currentdate.getDate()).slice(-2),
-  ].join("-");
-  const time = [
-    ("0" + currentdate.getHours()).slice(-2),
-    ("0" + currentdate.getMinutes()).slice(-2),
-    ("0" + currentdate.getSeconds()).slice(-2),
-  ].join(":");
-  const formatedDate = date + " " + time;
-  return await fetch(`http://localhost:3001/categories`, {
-    method: "POST",
-    headers: {
-      "Content-Type": "application/json",
-    },
-    body: JSON.stringify({ name: `${name}`, date: `${formatedDate}` }),
-  })
-    .then(() => "success")
-    .catch(() => "failed");
-};
-export const deleteCategory = async (id: string) => {
-  await fetch(`http://localhost:3001/categories/${id}`, {
-    method: "DELETE",
-  }).catch((error) => {
-    console.error("Error:", error);
-  });
-};
 export const sortData = (
   categoryData: Category[],
   orderBy: keyof Category,
