c116bc547e1d1d8cc97f02e6221f13c183292ffc
Rema'a Bdair <57008402+RemaaBdair@users.noreply.github.com>
2020-05-31 17:07:59 +0300

    use date-fns fucntions to compare between dates

--- src/pages/Products/util.tsx
+++ src/pages/Products/util.tsx
@@ -1,3 +1,5 @@
+import { isBefore, isAfter } from "date-fns/esm";
+
 export interface Product {
   id: string;
   code: string;
@@ -28,21 +30,29 @@ export const sortData = (
 type FilterOptions = "less than or equal" | "more than or equal";
 interface Filter {
   option: FilterOptions;
-  value: string | null;
+  value: string | Date | null;
   key: keyof Product;
 }
 const evaluate = (
   option: FilterOptions,
-  value: string | null,
+  value: string | Date | null,
   key: keyof Product,
   row: Product
 ): boolean => {
   let results = {
     "less than or equal": function () {
-      return value ? row[key].toLowerCase() <= value.toLowerCase() : true;
+      return value instanceof Date
+        ? isBefore(new Date(row[key]), value)
+        : value
+        ? row[key].toLowerCase() <= value.toLowerCase()
+        : true;
     },
     "more than or equal": function () {
-      return value ? row[key].toLowerCase() >= value.toLowerCase() : true;
+      return value instanceof Date
+        ? isAfter(new Date(row[key]), value)
+        : value
+        ? row[key].toLowerCase() >= value.toLowerCase()
+        : true;
     },
   };
   if (row[key]) return results[option]();
