8ef803a01a2a34804b46f6b9a6f7ffe48a0202bb
Rema'a Bdair <57008402+RemaaBdair@users.noreply.github.com>
2020-06-03 08:46:54 +0300

    move SortableContext to a new file

--- src/pages/Products/Components/ProductsList/ProductsList.tsx
+++ src/pages/Products/Components/ProductsList/ProductsList.tsx
@@ -5,7 +5,7 @@ import Table from "@material-ui/core/Table";
 import TableBody from "@material-ui/core/TableBody";
 import TableContainer from "@material-ui/core/TableContainer";
 import TablePagination from "@material-ui/core/TablePagination";
-import { Product, sortData, Order } from "../../util";
+import { Product, sortData } from "../../util";
 import { deleteProduct } from "../../api";
 import useSort from "../../../../hooks/useSort";
 import usePagination from "../../../../hooks/usePagination";
@@ -14,19 +14,12 @@ import useDeleteDialog from "../../../../hooks/useDeleteDialog";
 import { CustomizedTableHeader } from "./CustomizedTableHeader";
 import { CustomizedTableBody } from "./CustomizedTableBody";
 import DeleteDialog from "../../../../Components/DeleteDialog/DeleteDialog";
+import { SortTableContext } from "./context";
 interface Props {
   searchText: string;
   productsData: Product[];
   onFetchProducts: () => void;
 }
-export const SortTableContext = React.createContext<{
-  order: Order;
-  orderBy: keyof Product;
-  handleSort: (
-    event: React.MouseEvent<unknown, MouseEvent>,
-    property: keyof Product
-  ) => void;
-}>({ order: "asc", orderBy: "code", handleSort: () => {} });
 const ProductsList: React.FunctionComponent<
   WithStyles<typeof styles> & Props
 > = (props) => {
