cd3fa0ff45f2a5c0d909c031bfa039f16997c9f8
Rema'a Bdair <57008402+RemaaBdair@users.noreply.github.com>
2020-06-03 08:29:03 +0300

    add styling to ProductFormDialog component

---
+++ src/pages/Products/Components/ProductFormDialog/styles.tsx
@@ -0,0 +1,10 @@
+import { createStyles } from "@material-ui/core/styles";
+export const styles = createStyles({
+  title: {
+    backgroundColor: "#1861ab",
+    color: "white",
+  },
+  content: {
+    padding: 0,
+  },
+});
