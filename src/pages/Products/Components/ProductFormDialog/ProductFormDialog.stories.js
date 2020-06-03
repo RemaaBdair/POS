0e7628b16f994e001c3d268943cb5ed79f81182b
Rema'a Bdair <57008402+RemaaBdair@users.noreply.github.com>
2020-06-03 08:32:11 +0300

    add ProductFormDialog to storybook

---
+++ src/pages/Products/Components/ProductFormDialog/ProductFormDialog.stories.js
@@ -0,0 +1,8 @@
+import React from "react";
+import ProductFormDialog from "./ProductFormDialog";
+
+export default {
+  title: "Dialogs",
+};
+
+export const productFormDialog = () => <ProductFormDialog openDialog={true} />;
