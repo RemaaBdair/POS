20138e862d2a97930a9212e94c5983087c33e492
Rema'a Bdair <57008402+RemaaBdair@users.noreply.github.com>
2020-06-14 14:04:31 +0300

    add action types to the actions

--- src/actions/index.tsx
+++ src/actions/index.tsx
@@ -1,9 +1,11 @@
 import { createAction } from "redux-act";
 import { Product } from "../pages/Products/util";
+import { Category } from "../pages/Category/util";
+
 export const addItem = createAction<Product>("add item to the cart");
-export const fetchProductsResult = createAction(
+export const fetchProductsResult = createAction<Product[]>(
   "FETCH_ALL_PRODUCTS_ON_SUCCESS"
 );
-export const fetchCategoriesResult = createAction(
+export const fetchCategoriesResult = createAction<Category[]>(
   "FETCH_ALL_CATEGORIES_ON_SUCCESS"
 );
