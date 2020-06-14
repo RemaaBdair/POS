dd9c97ee80b338657cdc76a8b39a7cd365e2b6e5
Rema'a Bdair <57008402+RemaaBdair@users.noreply.github.com>
2020-06-14 08:18:14 +0300

    create POSpage

---
+++ src/pages/POS/POSPage.tsx
@@ -0,0 +1,19 @@
+import React from "react";
+import Grid from "@material-ui/core/Grid";
+import { RouteComponentProps } from "@reach/router";
+import { WithStyles, withStyles } from "@material-ui/core/styles";
+import StockItems from "./Components/StockItems/StockItems";
+import { styles } from "./styles";
+const POSPage: React.FunctionComponent<
+  WithStyles<typeof styles> & RouteComponentProps
+> = (props) => {
+  const { classes } = props;
+  return (
+    <Grid container className={classes.container}>
+      <Grid item xs={6} justify="flex-end" container>
+        <StockItems />
+      </Grid>
+    </Grid>
+  );
+};
+export default withStyles(styles)(POSPage);
