2043269915b78d5ea98a5888d865a0b4f0964ae1
Rema'a Bdair <57008402+RemaaBdair@users.noreply.github.com>
2020-06-14 14:14:14 +0300

    move State interface and default state to Store

--- src/store/index.tsx
+++ src/store/index.tsx
@@ -1,19 +1,30 @@
 import { createStore, applyMiddleware, compose } from "redux";
 import createSagaMiddleware from "redux-saga";
-import reducer from "../reducers/";
-import { rootSaga } from "./saga";
+import { rootReducer } from "../reducers/";
+import { rootSaga } from "../sagas";
+import { Product } from "../pages/Products/util";
+import { Category } from "../pages/Category/util";
 
 declare global {
   interface Window {
     __REDUX_DEVTOOLS_EXTENSION_COMPOSE__?: typeof compose;
   }
 }
-
+export interface State {
+  items: Product[];
+  products: Product[];
+  categories: Category[];
+}
+export const defaultState = {
+  items: [],
+  products: [],
+  categories: [],
+};
 const sagaMiddleware = createSagaMiddleware();
 const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
 
 const store = createStore(
-  reducer,
+  rootReducer,
   composeEnhancers(applyMiddleware(sagaMiddleware))
 );
 
