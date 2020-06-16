13d6da02c753bb01fed556c0c7abebb3e923b7ac
Rema'a Bdair <57008402+RemaaBdair@users.noreply.github.com>
2020-06-16 13:58:06 +0300

    add new function to sagss

--- src/sagas/index.tsx
+++ src/sagas/index.tsx
@@ -1,8 +1,9 @@
 import { takeLatest } from "redux-saga/effects";
 import { fetchProductList } from "./products";
 import { fetchCategoryList } from "./categories";
-
+import { doPaymentFn } from "./payment";
 export function* rootSaga() {
   yield takeLatest("FETCH_ALL_PRODUCTS_REQUEST", fetchProductList);
   yield takeLatest("FETCH_ALL_CATEGORIES_REQUEST", fetchCategoryList);
+  yield takeLatest("PAY", doPaymentFn);
 }
