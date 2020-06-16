d62cf31ce4f29645a3041c25c3519d3be9fa0874
Rema'a Bdair <57008402+RemaaBdair@users.noreply.github.com>
2020-06-16 15:06:09 +0300

    lower case actions type

--- src/actions/index.tsx
+++ src/actions/index.tsx
@@ -2,15 +2,15 @@ import { createAction } from "redux-act";
 import { Product } from "../pages/Products/util";
 import { Category } from "../pages/Category/util";
 
-export const addProduct = createAction<Product>("ADD_PRODUCT_TO_THE_CART");
-export const fetchProductsResult = createAction<Product[]>(
-  "FETCH_ALL_PRODUCTS_ON_SUCCESS"
+export const addProduct = createAction<Product>("add product to cart");
+export const fetchAllProducts = createAction<Product[]>(
+  "fetch products success"
 );
-export const fetchCategoriesResult = createAction<Category[]>(
-  "FETCH_ALL_CATEGORIES_ON_SUCCESS"
+export const fetchAllCategories = createAction<Category[]>(
+  "fetch categories success"
 );
-export const incQuantity = createAction<string>("INCREMENT_PRODUCT_COUNT");
-export const decQuantity = createAction<string>("DECCREMENT_PRODUCT_COUNT");
-export const deleteProduct = createAction<string>("DELETE_PRODUCT_FROM_CART");
-export const deleteCart = createAction("DELETE_CART");
-export const pay = createAction("PAY");
+export const incQuantity = createAction<string>("increment product count");
+export const decQuantity = createAction<string>("decrement product count");
+export const deleteProduct = createAction<string>("delete product from cart");
+export const deleteCart = createAction("delete cart");
+export const pay = createAction("pay");
