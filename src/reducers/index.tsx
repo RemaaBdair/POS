790b5f1654cec8b294fa370c7b806f5b1c25ac15
Rema'a Bdair <57008402+RemaaBdair@users.noreply.github.com>
2020-06-16 13:58:45 +0300

    modify root Reducer

--- src/reducers/index.tsx
+++ src/reducers/index.tsx
@@ -1,6 +1,11 @@
 import { combineReducers } from "redux";
 import products from "./products";
 import categories from "./categories";
+import cart from "./cart";
 import items from "./items";
-
-export const rootReducer = combineReducers({ products, categories, items });
+export const rootReducer = combineReducers({
+  allProducts: products,
+  categories: categories,
+  cart: cart,
+  products: items,
+});
