851bd661e5a5645da531468c4977495c3f1ab3b5
Rema'a Bdair <57008402+RemaaBdair@users.noreply.github.com>
2020-06-16 14:00:12 +0300

    modify categories reducer

--- src/reducers/categories.tsx
+++ src/reducers/categories.tsx
@@ -1,13 +1,8 @@
 import { createReducer } from "redux-act";
 import { fetchCategoriesResult } from "../actions";
-import { State, defaultState } from "../store";
 import { Category } from "../pages/Category/util";
-const reducer = createReducer<State>({}, defaultState);
-reducer.on(fetchCategoriesResult, (state: State, payload: Category[]) => {
-  return {
-    ...state,
-
-    categories: payload,
-  };
+const reducer = createReducer<Category[]>({}, []);
+reducer.on(fetchCategoriesResult, (state, payload: Category[]) => {
+  return payload;
 });
 export default reducer;
