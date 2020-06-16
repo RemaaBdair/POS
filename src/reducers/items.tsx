78b42579a17046b62f058629995af8ddbd81f2cb
Rema'a Bdair <57008402+RemaaBdair@users.noreply.github.com>
2020-06-16 14:00:27 +0300

    modify item reducer

--- src/reducers/items.tsx
+++ src/reducers/items.tsx
@@ -1,12 +1,35 @@
 import { createReducer } from "redux-act";
-import { addItem } from "../actions";
-import { State, defaultState } from "../store";
+import { addProduct, deleteProduct, deleteCart } from "../actions";
+import { Product } from "../pages/Products/util";
 
-const reducer = createReducer<State>({}, defaultState);
-reducer.on(addItem, (state, payload) => ({
-  ...state,
+interface state {
+  [id: string]: { price: number; name: string };
+}
+const reducer = createReducer<state>({}, {} as any);
+reducer.on(addProduct, (state, payload: Product) => {
+  let isFound: boolean = false;
+  for (let x in state) {
+    if (x === payload.id) {
+      isFound = true;
+    }
+  }
+  if (!isFound) {
+    state[payload.id] = {
+      name: payload.name,
+      price: +payload.price.slice(0, -1),
+    };
+  }
 
-  items: [...state.items, payload],
-}));
-
+  return state;
+});
+reducer.on(deleteProduct, (state, payload: string) => {
+  const products = state;
+  for (let x in products) {
+    if (x === payload) delete products[x];
+  }
+  return products;
+});
+reducer.on(deleteCart, (state) => {
+  return [] as any;
+});
 export default reducer;
