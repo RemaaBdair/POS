3a9a8024106d486c8028924192116dac53aff981
Rema'a Bdair <57008402+RemaaBdair@users.noreply.github.com>
2020-06-16 14:10:41 +0300

    adding Cart to POS page

--- src/pages/POS/POSPage.tsx
+++ src/pages/POS/POSPage.tsx
@@ -3,6 +3,8 @@ import Grid from "@material-ui/core/Grid";
 import { RouteComponentProps } from "@reach/router";
 import { WithStyles, withStyles } from "@material-ui/core/styles";
 import StockItems from "./Components/StockItems/StockItems";
+import Cart from "./Components/Cart/Cart";
+
 import { styles } from "./styles";
 const POSPage: React.FunctionComponent<
   WithStyles<typeof styles> & RouteComponentProps
@@ -10,6 +12,9 @@ const POSPage: React.FunctionComponent<
   const { classes } = props;
   return (
     <Grid container className={classes.container}>
+      <Grid item xs={6} justify="flex-start" container>
+        <Cart />
+      </Grid>
       <Grid item xs={6} justify="flex-end" container>
         <StockItems />
       </Grid>
