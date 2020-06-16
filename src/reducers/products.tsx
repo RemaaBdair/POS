e6827cda081725af7e0a05b24ec5bbfd94c66b05
Rema'a Bdair <57008402+RemaaBdair@users.noreply.github.com>
2020-06-16 13:59:09 +0300

    modify products reducer

--- src/reducers/products.tsx
+++ src/reducers/products.tsx
@@ -1,14 +1,11 @@
 import { createReducer } from "redux-act";
 import { fetchProductsResult } from "../actions";
-import { State, defaultState } from "../store";
+import { State, defaultState } from "../store/state";
 import { Product } from "../pages/Products/util";
 
-const reducer = createReducer<State>({}, defaultState);
-reducer.on(fetchProductsResult, (state: State, payload: Product[]) => {
-  return {
-    ...state,
-
-    products: payload,
-  };
+const reducer = createReducer<Product[]>({}, []);
+reducer.on(fetchProductsResult, (state, payload: Product[]) => {
+  return payload;
 });
+
 export default reducer;
